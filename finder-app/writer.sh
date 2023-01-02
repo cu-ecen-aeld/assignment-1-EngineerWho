1cee9f40afeaf73042fbdcf34cba5a031e2a4ea6
EngineerWho <111682198+EngineerWho@users.noreply.github.com>
2023-01-02 17:45:35 +0300

    Update writer.sh

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,25 +1,33 @@
-#! /bin/bash
+#!/bin/sh
 
-writefile=$1
-writestr=$2
-
-
-
-if [[ "$writefile" == "" ]] || [[ "$writestr" == "" ]]; then
-    echo "Please enter the file path and the string to write"
-    exit 1
+#Exits with return value 1 error and print statements if any of the parameters above were not specified
+if [ $# -ne 2 ]
+then
+	echo "Required two parameters"
+	exit 1
 fi
 
-mkdir -p $(dirname "$writefile")
+WRITE_FILE=$1
+WRITE_STRING=$2
+WRITE_FILE_DIR_NAME="$(dirname ${WRITE_FILE})"
 
-if [ $? != 0 ]; then
-    echo "Error creating file"
-    exit 1
+echo "WRITE_FILE: ${WRITE_FILE}"
+echo "WRITE_STRING: ${WRITE_STRING}"
+echo "WRITE_FILE_DIR_NAME: ${WRITE_FILE_DIR_NAME}"
+
+if [ ! -z "${WRITE_FILE_DIR_NAME}" ] && [ ! -d "${WRITE_FILE_DIR_NAME}" ]
+then
+	mkdir -p "${WRITE_FILE_DIR_NAME}"
+	if [ $? -ne 0 ]
+	then
+		echo "Cannot create directory ${WRITE_FILE_DIR_NAME }"
+    	exit 1
+	fi
 fi
 
-echo "$writestr" > "$writefile"
-
-if [ $? != 0 ]; then
-    echo "Error writing to file"
-    exit 1
+echo ${WRITE_STRING} > ${WRITE_FILE} 
+if [ $? -ne 0 ]
+then
+	echo "Cannot modify file ${WRITE_FILE}"
+	exit 1
 fi
