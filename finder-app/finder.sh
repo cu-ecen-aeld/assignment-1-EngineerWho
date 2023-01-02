3bebea245dcd7f468f4a240f69f9010f488e9aad
EngineerWho <111682198+EngineerWho@users.noreply.github.com>
2023-01-02 17:11:02 +0300

    Update finder.sh

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,24 +1,21 @@
-#! /bin/bash
+#!/bin/bash
 
-filedir=$1
+filesdir=$1
 searchstr=$2
 
-if [ filedir == "" ]; then
-    echo "Please enter the directory path"
-    exit 1
+if [ -z $filesdir ] || [ -z $searchstr ]
+then
+	echo "filesdir and searchstr should be provided as args"
+	exit 1
 fi
 
-if [ searchstr == "" ]; then
-    echo "Please enter the string to search"
-    exit 1
+if [ ! -d $filesdir ]
+then
+	echo "filesdir is not a directory"
+	exit 1
 fi
 
-if [ ! -d "$filedir" ]; then
-    echo "Directory does not exist"
-    exit 1
-fi
+x=$(find $filesdir -type f | wc -l)
+y=$(grep -r $searchstr $filesdir | wc -l)
 
-num_all_files=$(find $filedir -type f | wc -l)
-num_matching_lines=$(grep -r $searchstr $filedir | wc -l)
-
-echo "The number of files are $num_all_files and the number of matching lines are $num_matching_lines"
+echo "The number of files are $x and the number of matching lines are $y"
