6f34e943f6a9b871f486594b6c6f9397da1aec2c
EngineerWho <111682198+EngineerWho@users.noreply.github.com>
2023-01-02 16:57:25 +0300

    Update writer.sh

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,25 +1,20 @@
-#! /bin/bash
+#!/bin/bash
 
-writefile=$1
-writestr=$2
-
-
-
-if [[ "$writefile" == "" ]] || [[ "$writestr" == "" ]]; then
-    echo "Please enter the file path and the string to write"
-    exit 1
+if [ $# -ne 2 ]; then
+  echo "Error: 2 arguments required"
+  exit 1
 fi
 
-mkdir -p $(dirname "$writefile")
+writedir="$1"
+writestr="$2"
 
-if [ $? != 0 ]; then
-    echo "Error creating file"
-    exit 1
+mkdir -p "$(dirname $writedir)"
+
+echo $writestr > $writedir
+
+if [ ! -f $writedir ]; then
+  echo "Error: Unable to create file"
+  exit 1
 fi
 
-echo "$writestr" > "$writefile"
-
-if [ $? != 0 ]; then
-    echo "Error writing to file"
-    exit 1
-fi
+echo "Successfully created file $writedir with content $writestr."
