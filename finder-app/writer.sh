0992659a7bfa5325ba7f944b543b46886d49e44d
EngineerWho <111682198+EngineerWho@users.noreply.github.com>
2023-01-02 20:00:37 +0300

    Update writer.sh

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
-# Write a shell script finder-app/writer.sh as described below
-# 
-# - Accepts the following arguments: the first argument is a full path to a
-# file (including filename) on the filesystem, referred to below as writefile;
-# the second argument is a text string which will be written within this file,
-# referred to below as writestr
-# 
-# - Exits with value 1 error and print statements if any of the arguments above
-# were not specified
-# 
-# - Creates a new file with name and path writefile with content writestr,
-# overwriting any existing file and creating the path if it doesn’t exist.
-# Exits with value 1 and error print statement if the file could not be
-# created.
 
-# Check command line arguments.
 if [ $# != 2 ]; then
 	echo "Usage: $0 <writefile> <writestr>"
 	exit 1
