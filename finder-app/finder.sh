c63ac44f47a7322a19c321d8f9ae10bfd1c6eac5
EngineerWho <111682198+EngineerWho@users.noreply.github.com>
2023-01-11 20:24:15 +0300

    Update finder.sh

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,28 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-#Accepts the following runtime arguments: the first argument is a path to a directory on the filesystem, 
-#referred to below as filesdir; the second argument is a text string which will be searched within these files, referred to below as searchstr
+filesdir=$1
+searchstr=$2
 
-#Exits with return value 1 error and print statements if any of the parameters above were not specified
-if [ $# -ne 2 ]
+if [[ $# -ne 2 ]]
 then
-	echo "Required two parameters"
+	echo "Usage: finder.sh <directory> <search string>"
 	exit 1
 fi
 
-DIR_NAME=$1
-MATCHING_STRING=$2
-
-#Exits with return value 1 error and print statements if filesdir does not represent a directory on the filesystem
-if [ ! -d "${DIR_NAME}" ]
+if ! [[ -d "$filesdir" ]]
 then
-	echo "Derectory ${DIR_NAME} is not found"
+	echo "$filesdir is not a directory"
 	exit 1
 fi
 
-NUMBER_OF_FILES="$(find "${DIR_NAME}" -type f | wc -l)"
-NUMBER_OF_MATCHING_LINES="$(grep -r "${MATCHING_STRING}" "${DIR_NAME}" | wc -l)"
+numfiles=$(find "$filesdir" -type f | wc -l)
+matchlines=$(find "$filesdir" -type f -exec grep ${searchstr} {} \; | wc -l)
 
-#Prints a message "The number of files are X and the number of matching lines are Y" where X is the number of files in the directory 
-#and all subdirectories and Y is the number of matching lines found in respective files.
-echo "The number of files are ${NUMBER_OF_FILES} and the number of matching lines are ${NUMBER_OF_MATCHING_LINES}"
+echo "The number of files are $numfiles and the number of matching lines are $matchlines"
